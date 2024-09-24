949270b844981fe962a346f397a8ac3ba527891b
Markel-Bradford <markel.bradford94@gmail.com>
2024-09-24 11:56:53 -0500

    initial commit. Started upload and parse creation

---
+++ src/app/components/UploadandParse.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React, {useState, useRef} from 'react'
+
+interface PowerballData {
+    numbers: number[];
+    powerball: number;
+}
+
+const UploadandParse: React.FC = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default UploadandParse
